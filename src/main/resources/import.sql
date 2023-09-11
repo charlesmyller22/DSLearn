67b9b47f1dec03551fa8e0a6daabe01eb2150793
Charles Pereira <charlesmyller@hotmail.com>
2023-09-11 19:30:04 +0200

    Offer and Resource added to project

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -14,3 +14,10 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3,2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3,3);
 
 INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp Java', 'https://workshopexperience.com/wp-content/uploads/2020/03/CURSO-DE-FOTOGRAFIA-PROFESIONAL-2.jpg', 'https://workshopexperience.com/wp-content/uploads/2020/03/CURSO-DE-FOTOGRAFIA-PROFESIONAL-2.jpg')
+
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-07-25T13:00:00Z', TIMESTAMP WITH TIME ZONE '2022-07-25T13:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-07-25T13:00:00Z', TIMESTAMP WITH TIME ZONE '2022-07-25T13:00:00Z', 1);
+
+INSERT INTO tb_resource(title, description, position, img_uri, type, offer_id) VALUES ('Springboot', 'Aprenda Springboot', 1, 'https://workshopexperience.com/wp-content/uploads/2020/03/CURSO-DE-FOTOGRAFIA-PROFESIONAL-2.jpg', 1, 1);
+INSERT INTO tb_resource(title, description, position, img_uri, type, offer_id) VALUES ('FORUM', 'Tire suas duvidas', 2, 'https://workshopexperience.com/wp-content/uploads/2020/03/CURSO-DE-FOTOGRAFIA-PROFESIONAL-2.jpg', 2, 1);
+INSERT INTO tb_resource(title, description, position, img_uri, type, offer_id) VALUES ('Lives', 'Conteudo', 3, 'https://workshopexperience.com/wp-content/uploads/2020/03/CURSO-DE-FOTOGRAFIA-PROFESIONAL-2.jpg', 0, 1);
